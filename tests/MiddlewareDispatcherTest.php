2387f63381144d3348a9ed25fbe5bf028f13584e
Konstantin Baryshnikov <kbaryshnikov@upgate.co>
2015-11-01 14:03:05 +0300

    MiddlewareDispatcher test

---
+++ tests/MiddlewareDispatcherTest.php
@@ -0,0 +1,123 @@
+<?php
+
+use Illuminate\Contracts\Container\Container;
+use Upgate\LaravelJsonRpc\Server\MiddlewareDispatcher;
+use Upgate\LaravelJsonRpc\Contract\MiddlewaresConfiguration;
+
+class MiddlewareDispatcherTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testNoMiddlewares()
+    {
+        $container = $this->getMockBuilder(Container::class)->getMock();
+        /** @var Container $container */
+
+        $middlewares = $this->getMockBuilder(MiddlewaresConfiguration::class)->getMock();
+        $middlewares->method('getMiddlewares')->willReturn([]);
+        /** @var MiddlewaresConfiguration $middlewares */
+
+        $middlewareDispatcher = new MiddlewareDispatcher($container);
+        $result = $middlewareDispatcher->dispatch(
+            $middlewares,
+            42,
+            function ($context) {
+                return $context;
+            }
+        );
+        $this->assertEquals(42, $result);
+    }
+
+    public function testMiddlewaresChain()
+    {
+        $container = $this->getMockBuilder(Container::class)->getMock();
+        $container->method('make')->will(
+            $this->returnCallback(
+                function ($className) {
+                    return new $className;
+                }
+            )
+        );
+        /** @var Container $container */
+
+        $middlewares = $this->getMockBuilder(MiddlewaresConfiguration::class)->getMock();
+        $middlewares->method('getMiddlewares')->willReturn([
+            MiddlewareDispatcherTest_MiddlewareIncrement::class,
+            MiddlewareDispatcherTest_MiddlewareDouble::class,
+        ]);
+        /** @var MiddlewaresConfiguration $middlewares */
+
+        $middlewareDispatcher = new MiddlewareDispatcher($container);
+        $result = $middlewareDispatcher->dispatch(
+            $middlewares,
+            42,
+            function ($context) {
+                return -$context;
+            }
+        );
+        $this->assertEquals($result, -(42 + 1) * 2);
+    }
+
+    public function testMiddlewaresAbortedChain()
+    {
+        $container = $this->getMockBuilder(Container::class)->getMock();
+        $container->method('make')->will(
+            $this->returnCallback(
+                function ($className) {
+                    return new $className;
+                }
+            )
+        );
+        /** @var Container $container */
+
+        $middlewares = $this->getMockBuilder(MiddlewaresConfiguration::class)->getMock();
+        $middlewares->method('getMiddlewares')->willReturn([
+            MiddlewareDispatcherTest_MiddlewareIncrement::class,
+            MiddlewareDispatcherTest_MiddlewareReturn::class,
+            MiddlewareDispatcherTest_MiddlewareException::class,
+        ]);
+        /** @var MiddlewaresConfiguration $middlewares */
+
+        $middlewareDispatcher = new MiddlewareDispatcher($container);
+        $result = $middlewareDispatcher->dispatch(
+            $middlewares,
+            42,
+            function ($context) {
+                throw new \LogicException('Must never reach here. Context: ' . $context);
+            }
+        );
+        $this->assertEquals($result, 42 + 1);
+    }
+
+}
+
+class MiddlewareDispatcherTest_MiddlewareIncrement {
+
+    public function handle($context, callable $next) {
+        return $next($context + 1);
+    }
+
+}
+
+class MiddlewareDispatcherTest_MiddlewareDouble {
+
+    public function handle($context, callable $next) {
+        return $next($context * 2);
+    }
+
+}
+
+class MiddlewareDispatcherTest_MiddlewareReturn {
+
+    public function handle($context) {
+        return $context;
+    }
+
+}
+
+class MiddlewareDispatcherTest_MiddlewareException {
+
+    public function handle() {
+        throw new \LogicException(__CLASS__ . ' reached');
+    }
+
+}
