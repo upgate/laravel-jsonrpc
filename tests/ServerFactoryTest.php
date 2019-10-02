593bca1116c4dfb3db8730909aa43ff5a93445cb
Konstantin Baryshnikov <mail@kbaryshnikov.ru>
2019-10-02 03:54:48 +0300

    Server Factory

---
+++ tests/ServerFactoryTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Contracts\Container\Container;
+use Psr\Log\LoggerInterface;
+use Upgate\LaravelJsonRpc\Server;
+use Upgate\LaravelJsonRpc\ServiceProvider\JsonRpcServerFactory;
+
+class ServerFactoryTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testFactoryMakesNewInstancesOnEachCall()
+    {
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+
+        $container = $this->getMockBuilder(Container::class)->getMock();
+        $container->method('make')->with(LoggerInterface::class)->willReturn($logger);
+        /** @var Container $container */
+
+        $factory = new JsonRpcServerFactory($container);
+        $server1 = $factory->make();
+        $this->assertInstanceOf(Server\Server::class, $server1);
+        $server2 = $factory->make();
+        $this->assertInstanceOf(Server\Server::class, $server2);
+        $this->assertNotSame($server2, $server1);
+    }
+
+}
